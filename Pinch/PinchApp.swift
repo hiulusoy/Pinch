de96c79873463b2167162f454e390b522e3b0c82
hiusanmaz <hiusanmaz34@gmail.com>
2022-06-13 09:50:01 +0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Halil Usanmaz on 13.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
