857992903877d16634006bf17b985496822a222b
hiusanmaz <hiusanmaz34@gmail.com>
2022-06-21 21:09:24 +0300

    onTap gesture && resetImageState method

--- Pinch/ContentView.swift
+++ Pinch/ContentView.swift
@@ -12,18 +12,28 @@ struct ContentView: View {
     
     @State private var isAnimating: Bool = false;
     @State private var imageScale: CGFloat = 1;
+    @State private var imageOffset: CGSize = .zero;
+    
+    
     
     
     
     
     // MARK: - FUNCTION
     
+    func resetImageState (){
+        return withAnimation(.spring()){
+            imageScale = 1;
+            imageOffset = .zero;
+        }
+    }
+    
     // MARK: - CONTENT
     
     var body: some View {
-        NavigationView{
+        NavigationView {
             ZStack {
-            // MARK: - PAGE IMAGE
+                // MARK: - PAGE IMAGE
                 Image("magazine-front-cover")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
@@ -31,6 +41,7 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .animation(.linear(duration: 1), value: isAnimating)
                     .scaleEffect(imageScale)
                 // MARK: -1 TAP GESTURE
@@ -41,9 +52,18 @@ struct ContentView: View {
                             }
                         }
                         else{
-                            withAnimation(.spring()){
-                                imageScale = 1
-                            }
+                            resetImageState();
+                        }
+                    })
+                // MARK: -2 DRAG GESTURE
+                    .gesture(DragGesture().onChanged {
+                        value in withAnimation(.linear(duration: 1)){
+                            imageOffset = value.translation
+                        }
+                        
+                    }.onEnded { _ in
+                        if imageScale <= 1 {
+                            resetImageState();
                         }
                     })
             } //: ZSTACK
